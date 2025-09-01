0eb234f78a67e7abd18463972832d3278ac7d15f
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-09-01 13:07:24 -0700

    Vercel test

--- main.py
+++ main.py
@@ -9,23 +9,57 @@ app = FastAPI(
     version="1.0.0",
 )
 
-# Define request schema
+
+# Define request schema: 
+# It’s how you define the shape of the data your API expects
+
+# Valid Request:
+# {
+#   "nums": [2, 7, 11, 15],
+#   "target": 9
+# }
+
+# 400 Bad Request:
+# {
+#   "nums": "not a list",
+#   "target": "hello"
+# }
+
 class TwoSumRequest(BaseModel):
     nums: List[int]
     target: int
 
-# Define response schema
+
+
+# Define response schema: 
+# It defines the shape of the data your API will return
+
+# Example when a solution exists:
+
+# {
+#   "result":
+#   "error":
+# } 
+
 class TwoSumResponse(BaseModel):
     result: Optional[List[int]] = None
     error: Optional[str] = None
 
+
+# FastAPI route handler / endpoint function
+
 @app.post("/two_sum", response_model=TwoSumResponse)
+# Whenever someone sends a POST request to the path /two_sum, run the function below
+
 def two_sum(req: TwoSumRequest):
     """Solve the Two Sum problem as an API endpoint"""
-    indices = {}
+
+    # Add your LeetCode Logic in here!
+    seen = {}  # value → index
+        
     for i, num in enumerate(req.nums):
         complement = req.target - num
-        if complement in indices:
-            return {"result": [indices[complement], i]}
-        indices[num] = i
-    return {"error": "No two sum solution found"}
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    # return {"error": "No two sum solution found"}
