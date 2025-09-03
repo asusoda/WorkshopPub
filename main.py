f27dfc538d0e3d435259a8c54119b68eb6dbd1df
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-09-02 19:41:26 -0700

    Revert "Add 2 sum"
    This reverts commit 0bb70fe7db7907b5eae84cb36eaed1e549a3855f.

--- main.py
+++ main.py
@@ -25,9 +25,11 @@ app = FastAPI(
 #   "target": "hello"
 # }
 
-class TwoSumRequest(BaseModel):
-    nums: List[int]
-    target: int 
+# class TwoSumRequest(BaseModel):
+#     nums:
+#     target:
+
+
 
 # Define response schema: 
 # It defines the shape of the data your API will return
@@ -39,27 +41,20 @@ class TwoSumRequest(BaseModel):
 #   "error": null
 # }
 
-class TwoSumResponse(BaseModel):
-    result: List[int]
-    error: str
+# class TwoSumResponse(BaseModel):
+#     result: 
+#     error: 
 
 
 # FastAPI route handler / endpoint function
 
-@app.post("/two_sum", response_model=TwoSumResponse)
+# @app.post("/two_sum", response_model=TwoSumResponse)
 
 # Whenever someone sends a POST request to the path /two_sum, run the function below
 
-def two_sum(req: TwoSumRequest):
-    """Solve the Two Sum problem as an API endpoint"""
-    seen = {}
-    for i in range(len(req.nums)):
-        remainder = req.target - req.nums[i]
-        if remainder in seen:
-            return {
-                "result": [seen[remainder], i],
-                "error" : ""
-            }
-        seen[req.nums[i]] = i
-        
-    return {"error": "No two sum solution found"}
+# def two_sum(req: TwoSumRequest):
+#     """Solve the Two Sum problem as an API endpoint"""
+
+    # # Add your LeetCode Logic in here!
+    
+    # return {"error": "No two sum solution found"}
